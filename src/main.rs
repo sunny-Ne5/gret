bfe6ca93aa4f514197e90e7ac6dbefa4ebec8b8c
sunny-Ne5 <sunny.tiwari505@gmail.com>
2021-12-05 23:39:01 +0530

    Add first working model
    1. Use bufreader for efficient reading.
    2. Handle error with match case.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-struct Cli{
-    #[structopt(short)]
+struct Cli {
     pattern: String,
-    #[structopt(parse(from_os_str),help = "Path shall be passed")]
+    #[structopt(parse(from_os_str), help = "Path shall be passed")]
     path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::from_args();
+    let f = File::open(&args.path).expect("couldn't open file");
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        match line {
+            Ok(data) => {
+                if data.contains(&args.pattern) {
+                    println!("{}", data);
+                }
+            }
+            Err(err) => println!("Error: {}", err),
+        }
+    }
 }
