a00186c674236f727b64204bf77b04f1e5ddc5d6
sunny-Ne5 <sunny.tiwari505@gmail.com>
2021-12-08 23:27:29 +0530

    Add error handling for library function check_match

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use anyhow::Context;
 pub fn check_match(line: &String, pattern: &str, mut writer: impl std::io::Write) {
     if line.contains(pattern) {
-        writeln!(writer, "{}", line); //write bytes into writer
+        writeln!(writer, "{}", line).with_context(|| format!("couldn't write data to output"));
     }
 }
